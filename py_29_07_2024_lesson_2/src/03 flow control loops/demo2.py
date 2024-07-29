bd3f7c2b8fdc219de2ec4bd655e3508aa345d80d
Eldar <eldarba@gmail.com>
2024-07-29 22:36:22 +0300

    lesson 2

--- py_29_07_2024_lesson_2/src/03 flow control loops/demo2.py
+++ py_29_07_2024_lesson_2/src/03 flow control loops/demo2.py
@@ -1,15 +1,16 @@
 secret_password = "123"
-trials = 1
 
+trials = 1
 user_input = input("enter password to login: ")
 
 while trials < 3 and user_input != secret_password:
-    print("wrong password. try again")
-    user_input = input("enter password to login: ")
     trials = trials + 1
+    print("wrong password. try again (attempt", trials, "of 3)")
+    user_input = input("enter password to login: ")
 
 if user_input == secret_password:
     print("Good! you are logged in")
 else:
     print("login failed")
 
+print("number of trials:", trials)
