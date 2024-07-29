413bdb0db6b55c538b59ec91d3ad19e18a116c0c
Eldar <eldarba@gmail.com>
2024-07-29 18:00:54 +0300

    lesson 2

---
+++ py_29_07_2024_lesson_2/src/homework/tar2.py
@@ -0,0 +1,12 @@
+length = float(input("enter length: "))
+width = float(input("enter width: "))
+
+print("length:", length, ", width:", width)
+
+area = length * width
+print("area:", area)
+
+perimeter = 2 * (length + width)
+print("perimeter:", perimeter)
+
+
---
+++ py_29_07_2024_lesson_2/src/homework/tar3.py
@@ -0,0 +1,5 @@
+import random
+
+r = random.randint(0, 100)
+print(r)
+print("is even?", r % 2 == 0)
