caab0e5f22d69053f285dadfaa3a0558bf2ff5c7
Eldar <eldarba@gmail.com>
2024-07-29 17:50:16 +0300

    lesson 2

---
+++ py_29_07_2024_lesson_2/src/homework/tar1.py
@@ -0,0 +1,9 @@
+a = float(input("enter 1st number: "))
+b = float(input("enter 2nd number: "))
+c = float(input("enter 3rd number: "))
+
+the_sum = a + b + c
+print("sum =", the_sum)
+
+avg = the_sum / 3
+print("average =", avg)
