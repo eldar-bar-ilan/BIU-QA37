5ceca4d0ef3d49f211992aa82504056a17240953
Eldar <eldarba@gmail.com>
2024-08-06 17:06:24 +0300

    fix 1 - menu

--- my_application/app_core/menus.py
+++ my_application/app_core/menus.py
@@ -1,6 +1,6 @@
 def display_menu():
     print("\n1............... greet user")
     print("2............... sum of 2 numbers")
-    print("3............... add numbers")
+    print("3............... find maximum value")
     print("x............... exit\n")
 
