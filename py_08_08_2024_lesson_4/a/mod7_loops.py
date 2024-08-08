36585a8c99a2c8553908c80fbfc84e5614ae4602
Eldar <eldarba@gmail.com>
2024-08-08 20:04:24 +0300

    lesson 4

---
+++ py_08_08_2024_lesson_4/a/mod7_loops.py
@@ -0,0 +1,15 @@
+names = ["Alon", "Ariela", "Yosi", "Dana"]
+
+for name in names:
+    print(name)
+
+# creating ranges
+numbers1 = range(10)  # 0 - 9
+numbers2 = range(3, 10)  # 3 - 9
+numbers3 = range(3, 10, 2)  # 3, 5, 7, 9
+for x in numbers3:
+    print(x)
+
+print("===============")
+for x in range(200, 251, 4):
+    print(x)
