5e999215ae08e54064103a09eccf722b564f796d
Eldar <eldarba@gmail.com>
2024-08-08 18:42:42 +0300

    the work interview application

--- my_application/app_core/menus.py
+++ my_application/app_core/menus.py
@@ -2,5 +2,10 @@ def display_menu():
     print("\n1............... greet user")
     print("2............... sum of 2 numbers")
     print("3............... add numbers")
+    print("3............... find max")
+    print("4............... find circle area")
+    print("5............... convert fahrenheit to celsius")
+    print("find............ find word in dictionary")
+    print("add............. add entry to dictionary")
     print("x............... exit\n")
 
