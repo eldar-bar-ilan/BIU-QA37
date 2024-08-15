46bab6ca927985ebc79a14ed107fd21e8be68fb6
Eldar <eldarba@gmail.com>
2024-08-15 19:39:42 +0300

    db-api

--- py_15_08_2024_db_api/core/a/demo2.py
+++ py_15_08_2024_db_api/core/a/demo2.py
@@ -3,16 +3,19 @@ import mysql.connector as db
 # connection to a specified database
 con = db.connect(host='localhost', user='root', password='1234', database='db_python')
 cursor = con.cursor()
+cursor.execute("truncate table products")  # clear table data
 
 list_sql = [
-    "insert into products values(0, 'Radio', 230)",
-    "insert into products values(0, 'Milk', 8.30)",
-    "insert into products values(0, 'Bread', 12)",
-    "insert into products values(0, 'Car', 230000)",
-    "insert into products values(0, 'Airplane', 25000000)",
+    "(0, 'Radio', 230)",
+    "(0, 'Milk', 8.30)",
+    "(0, 'Bread', 12)",
+    "(0, 'Car', 230000)",
+    "(0, 'Airplane', 25000000)",
 ]
 
 for sql in list_sql:
-    cursor.execute(sql)
+    cursor.execute(f'insert into products values{sql}')
+    print(f'insert into products values{sql}')
 
 con.commit()
+con.close()  # closing connection is important to not wast resources
