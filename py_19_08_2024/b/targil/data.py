df107adb2088d835230be88ba9c66fe33118298f
Eldar <eldarba@gmail.com>
2024-08-19 22:04:31 +0300

    python resource management

--- py_19_08_2024/b/targil/data.py
+++ py_19_08_2024/b/targil/data.py
@@ -16,11 +16,11 @@ def add_book(book: Book):
     with db.connect(host='localhost', user='root', password='1234', database='db_library') as con:
         # connect to the db_library db
         # get a cursor
-        cursor_ = con.cursor(prepared=True)
+        cursor = con.cursor(prepared=True)
         # define an sql command
         sql = f'insert into books values(0, %s, %s)'
         # execute the command
-        cursor_.execute(sql, (book.title, book.author))
+        cursor.execute(sql, (book.title, book.author))
         # use the connection to commit
         con.commit()
         # close the connection (unless you used with)
@@ -62,15 +62,19 @@ def clear_all_books():
         con.commit()
 
 
-with db.connect(host='localhost', user='root', password='1234') as con:
-    cursor = con.cursor()
-    # create a database named db_library
-    cursor.execute('create database if not exists db_library')
-    cursor.execute('use db_library')
-    # create a table named books with isbn, title, author
-    cursor.execute('''
-    create table if not exists books(
-    isbn int primary key auto_increment,
-    title varchar(50),
-    author varchar(30)
-    )''')
+def build_db():
+    with db.connect(host='localhost', user='root', password='1234') as con:
+        cursor = con.cursor()
+        # create a database named db_library
+        cursor.execute('create database if not exists db_library')
+        cursor.execute('use db_library')
+        # create a table named books with isbn, title, author
+        cursor.execute('''
+        create table if not exists books(
+        isbn int primary key auto_increment,
+        title varchar(50),
+        author varchar(30)
+        )''')
+
+
+build_db()
