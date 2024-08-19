c3e25da9c276ac6273d285b0b332876b26e158c8
Eldar <eldarba@gmail.com>
2024-08-19 17:46:56 +0300

    python resource management

---
+++ py_19_08_2024/a/resources/demo1_resources.py
@@ -0,0 +1,12 @@
+class Car:
+    def __init__(self):
+        self.speed = 0
+
+    def drive(self, speed):
+        self.speed = speed
+
+    def chek_oil(self):
+        print('checking oil')
+
+    def turn_lights_off(self):
+        print('lights turned off')
