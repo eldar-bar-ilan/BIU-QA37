2919c0494cc49605b6e2503a40d071ab6e98df25
Eldar <eldarba@gmail.com>
2024-08-22 20:13:08 +0300

    unit testing

--- py_22_08_2024_unit_testing/test_employee.py
+++ py_22_08_2024_unit_testing/test_employee.py
@@ -14,3 +14,14 @@ class TestEmployee(unittest.TestCase):
         emp_2.first = 'Ran'
         self.assertEqual(emp_1.email, 'dan.smith@email.com')
         self.assertEqual(emp_2.email, 'ran.levin@email.com')
+
+    def test_full_name(self):
+        emp_1 = Employee('John', 'Smith', 30_000)
+        emp_2 = Employee('Mark', 'Levin', 50_000)
+        self.assertEqual(emp_1.full, 'John Smith')
+        self.assertEqual(emp_2.full, 'Mark Levin')
+        # change the first name and check again
+        emp_1.first = 'Dan'
+        emp_2.first = 'Ran'
+        self.assertEqual(emp_1.full, 'Dan Smith')
+        self.assertEqual(emp_2.full, 'Ran Levin')
