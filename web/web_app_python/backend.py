7b15e9b881b90e989c47d7a3678abe756ce8a0a6
Eldar <eldarba@gmail.com>
2024-08-29 20:21:12 +0300

    auto commit batch

--- web/web_app_python/backend.py
+++ web/web_app_python/backend.py
@@ -3,16 +3,17 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'post':
+    if request.method == 'POST':
         # if we are here we have client data posted
         first_name = request.form['first']
         last_name = request.form['last']
         response_msg = f'Hello {first_name} {last_name}!'
-        return response_msg
+        return render_template('response.html', message=response_msg)
     else:
         # if we are here we send the form to the client
-        render_template('index.html')
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
