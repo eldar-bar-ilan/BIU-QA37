8d76c252ee61a59965c5e3e890162ab2874e05e4
Eldar <eldarba@gmail.com>
2024-09-09 20:32:42 +0300

    auto commit batch

---
+++ selenium/selenium_2024_09_09/b.dom/demo1_find_elements.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import time
+import os
+
+web_dir = os.getcwd() + '/../web'
+
+# url = 'file://C:/repos/BIU-QA37/selenium/selenium_2024_09_09/web/find-element.html'
+# driver = webdriver.Chrome()
+# driver.get(url)
+#
+# time.sleep(5)
+# driver.quit()
+
