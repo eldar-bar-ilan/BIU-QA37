6ebc53cab60097d521182dec46679783640a98b7
Eldar <eldarba@gmail.com>
2024-09-09 20:51:14 +0300

    auto commit batch

--- selenium/selenium_2024_09_09/b.dom/demo1_find_elements.py
+++ selenium/selenium_2024_09_09/b.dom/demo1_find_elements.py
@@ -1,13 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 import os
 
+# the [..] notation means - go up one directory
 web_dir = os.getcwd() + '/../web'
 
-# url = 'file://C:/repos/BIU-QA37/selenium/selenium_2024_09_09/web/find-element.html'
-# driver = webdriver.Chrome()
-# driver.get(url)
-#
-# time.sleep(5)
-# driver.quit()
+url = f'{web_dir}/find-element.html'
+driver = webdriver.Chrome()
+driver.get(url)
+time.sleep(3)
+
+# find the input element for first name - BY.ID
+web_element = driver.find_element(By.ID,'first-id')
+web_element.send_keys("Eldar")
+time.sleep(3)
+
+# find the input element for first name - BY.ID
+web_element = driver.find_element(By.NAME, 'last')
+web_element.send_keys('Bakshi')
+
+time.sleep(5)
+driver.quit()
 
