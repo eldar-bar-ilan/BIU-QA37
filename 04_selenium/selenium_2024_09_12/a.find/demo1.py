90bbe2233fb4bc0989c670b175c689a90d1d4ae3
Eldar <eldarba@gmail.com>
2024-09-12 18:11:56 +0300

    automated push

--- 04_selenium/selenium_2024_09_12/a.find/demo1.py
+++ 04_selenium/selenium_2024_09_12/a.find/demo1.py
@@ -6,13 +6,23 @@ import os
 
 driver = webdriver.Chrome()
 driver.get(f'file://{os.getcwd()}/../web/find_by_name.html')
+
+# find elements by tag name
 input_elements = driver.find_elements(By.TAG_NAME, 'input')
 time.sleep(1)
 input_elements[0].send_keys('Eldar')
 time.sleep(1)
 input_elements[1].send_keys('123')
-time.sleep(3)
+time.sleep(1)
 # input_elements[2].click()
 input_elements[3].click()
+time.sleep(1)
+
+# find a elements by text
+driver.find_element(By.LINK_TEXT, 'go to google').click()
 time.sleep(3)
+driver.back()
+time.sleep(3)
+
+
 driver.quit()
