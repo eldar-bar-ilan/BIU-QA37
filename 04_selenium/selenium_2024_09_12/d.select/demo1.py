45f53a2d598c2c323b1e14f8b728506eca06936d
Eldar <eldarba@gmail.com>
2024-09-12 21:08:29 +0300

    automated push

--- 04_selenium/selenium_2024_09_12/d.select/demo1.py
+++ 04_selenium/selenium_2024_09_12/d.select/demo1.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Chrome()
 driver.get(f'file://{os.getcwd()}/../web/select.html')
-time.sleep(3)
+time.sleep(2)
 select_element = driver.find_element(By.ID, 'color')
 select = Select(select_element)
 
@@ -17,7 +17,19 @@ for opt in options:
     print(opt.text)
 
 # select an option - by index
-select.select_by_index(2)
+select.select_by_index(2)  # green
+time.sleep(1)
+
+# select an option - by value
+select.select_by_value('black')  # black
+time.sleep(1)
+
+# select an option - by visible text
+select.select_by_visible_text('White')  # white
+time.sleep(1)
+
+# cancel selection
+select.select_by_index(0)
 time.sleep(3)
 
 driver.quit()
