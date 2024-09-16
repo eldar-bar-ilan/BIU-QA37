2366a174f59caca219aaa62044e3d2a999c53900
Eldar <eldarba@gmail.com>
2024-09-16 21:05:17 +0300

    auto commit batch

--- 04_selenium/automation_project_eldar/test_home.py
+++ 04_selenium/automation_project_eldar/test_home.py
@@ -1,20 +1,32 @@
 import unittest
+from selenium import webdriver
+# options let you add arguments on how to start the browser
+from selenium.webdriver.chrome.options import Options
+import os
+import time
 
 
 class TestHomePage(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
         # initial operation - open browser...
-        pass
+        # for maximized browser - create an Option object and add argument for maximized
+        options = Options()
+        options.add_argument('--start-maximized')
+        cls.driver = webdriver.Chrome(options=options)
+        # set the base url for all files if you want to write less code in the test methods
+        cls.base_url = f'file://{os.getcwd()}/web/'
 
     @classmethod
     def tearDownClass(cls):
         # final operations - closing browser, closing selenium driver, etc.
-        pass
+        time.sleep(3)
+        cls.driver.quit()
 
     #    test methods
 
     def test_page_title(self):
         # use selenium to open the home page and get the title
+        self.driver.get(f"{self.base_url}home.html")
         # use unittest to test the title
-        pass
